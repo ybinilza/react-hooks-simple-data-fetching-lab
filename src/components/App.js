32f381ca1ae6aae3da4382b9f7eaf7827f30365d
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-06-26 19:31:40 -0400

    Lab Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,32 @@
 // create your App component here
+import { useEffect, useState } from "react"
+import React  from 'react'
+
+
+
+function App() {
+
+const[image, setImage]= useState("")
+useEffect(()=>
+{   fetch("https://dog.ceo/api/breeds/image/random")
+        .then((r)=>r.json())
+          .then((data) => setImage(data.message))
+
+},[])
+
+if(!image) 
+{
+    return <p>Loading...</p>
+}
+
+  return (
+    <div>
+      <img
+       src= {image}
+       alt="A Random Dog"/>
+    </div>
+  )
+}
+
+export default App
+
